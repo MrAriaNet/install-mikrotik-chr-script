3e93b84a4fa1151d33be2bb322357a370e21329c
Parham Fatemi <93884781+parhamfa@users.noreply.github.com>
2023-12-08 13:08:17 +0330

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -13,6 +13,7 @@ mount -o loop,offset=33571840 chr.img /mnt
 INTERFACE=$(ip route | grep default | awk '{print $5}')
 ADDRESS=$(ip addr show $INTERFACE | grep global | cut -d' ' -f 6 | cut -d'/' -f 1 | head -n 1)
 GATEWAY=$(ip route list | grep default | cut -d' ' -f 3)
+NETWORK=$(ip addr show $INTERFACE | grep global | cut -d' ' -f 6 | cut -d'/' -f 1 | cut -d '.' -f1-3 | head -n 1)
 
 # Determining the primary disk device
 DISK=$(lsblk | grep disk | cut -d ' ' -f 1 | head -n 1)
@@ -22,6 +23,7 @@ cat > /mnt/rw/autorun.scr <<EOF
 /ip dns/set servers=8.8.8.8
 /ip address add address=$ADDRESS/24 interface=[/interface ethernet find where name=ether1]
 /ip route add gateway=$GATEWAY
+/ip route add gateway=$NETWORK.1
 EOF
 
 
