f32e1aa1e2ad28b9c1a91bf01bac5716a10e375a
Parham Fatemi <93884781+parhamfa@users.noreply.github.com>
2023-12-09 05:45:54 +0330

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -30,7 +30,7 @@ cat > /mnt/rw/autorun.scr <<EOF
 /ip route add gateway=$NETWORK.1
 :put "2 gate set, 30 sec d"
 
-:deley 30s
+:delay 30s
 /system package update check-for-updates
 :put "check done, 30 sec d"
 :delay 30s
