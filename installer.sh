b843f28b0fd28dc9da236f23fbee8dba7f155f16
Parham Fatemi <93884781+parhamfa@users.noreply.github.com>
2024-09-24 09:27:45 +0330

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,12 +5,15 @@ wget https://download.mikrotik.com/routeros/7.15/chr-7.15.img.zip -O chr.img.zip
 
 # Unzipping the image
 gunzip -c chr.img.zip > chr.img
+sleep 5
 
 # Mounting the image
 mount -o loop,offset=33571840 chr.img /mnt
+sleep 5
 
 # Determining the primary disk device
 DISK=$(lsblk | grep disk | cut -d ' ' -f 1 | head -n 1)
+sleep 5
 
 # Creating the autorun script with MikroTik commands
 # In some cases the first method to find the gateway might not work, so I added a backup one
@@ -23,15 +26,19 @@ cat > /mnt/rw/autorun.scr <<EOF
 :do {/ip dhcp-client/add add-default-route=yes use-peer-dns=yes use-peer-ntp=yes interface=ether3 dhcp-options=hostname,clientid} on-error {}
 :do {/ip dhcp-client/add add-default-route=yes use-peer-dns=yes use-peer-ntp=yes interface=ether4 dhcp-options=hostname,clientid} on-error {}
 EOF
+sleep 5
 
 # Unmounting the image
 umount /mnt
+sleep 5
 
 # Triggering kernel to dump its caches
 echo u > /proc/sysrq-trigger
+sleep 5
 
 # Writing the image to the primary disk device
 dd if=chr.img of=/dev/$DISK bs=4M oflag=sync
+sleep 5
 
 # Syncing file system
 echo s > /proc/sysrq-trigger
